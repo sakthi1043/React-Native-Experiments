acc7a79775b50fcfe32db15d14a1f347f2735746
sakthi1043 <sakthi1043@gmail.com>
2025-08-05 10:34:15 +0530

    10:33AM_05/08/25 Commit

--- FeedBack_Navigation/ThankYou.js
+++ FeedBack_Navigation/ThankYou.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,Image } from 'react-native';
 
 export default function ThankYouScreen({ route }) {
   const { feedback } = route.params || {};
 
   return (
     <View style={styles.container}>
+      <Image
+          source={require('./assets/free.webp')}
+          style={styles.logo}
+        />
       <Text style={styles.heading}>Thank You for your feedback!</Text>
       {feedback && (
         <View style={styles.summary}>
@@ -23,4 +27,11 @@ const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 },
   heading: { fontSize: 24, fontWeight: 'bold', marginBottom: 20, textAlign: 'center' },
   summary: { marginTop: 10, alignItems: 'center' },
+  logo:{
+    width:200,
+    height:200,
+    resizeMode:'contain',
+    alignSelf:'center',
+    marginBottom:20,
+  }
 });
