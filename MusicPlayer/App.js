96499e5e66583bcfcd049d9bd4a1749fd85d83a5
sakthi1043 <sakthi1043@gmail.com>
2025-09-11 12:50:36 +0530

    lab

--- MusicPlayer/App.js
+++ MusicPlayer/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Music from './Music';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Music/>
+      <Text>Hello</Text>
     </View>
   );
 }
---
+++ MusicPlayer/Music.js
@@ -0,0 +1,19 @@
+import TrackPlayer from 'react-native-track-player';
+
+const start = async () => {
+    // Set up the player
+    await TrackPlayer.setupPlayer();
+
+    // Add a track to the queue
+    await TrackPlayer.add({
+        id: 'trackId',
+        url: require('track.mp3'),
+        title: 'Track Title',
+        artist: 'Track Artist',
+        artwork: require('track.png')
+    });
+
+    // Start playing it
+    await TrackPlayer.play();
+};
+start();
